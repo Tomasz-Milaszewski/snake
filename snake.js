f26a9ecb202a02632d6181601b91d1743f552e88
Tomek_M <tomasz.milaszewski@gmail.com>
2018-12-06 09:06:38 +0100

    adds drawSnake function

--- snake.js
+++ snake.js
@@ -1,18 +1,25 @@
 (function game() {
-  const cellSize = 10;
-  const cellsGrid = 20;
-
-  // render snake
-  let snake = [{ x: 0, y: 0 }];
+  // canvas
   const canvas = document.querySelector("#canvas");
   const ctx = canvas.getContext("2d");
-  ctx.fillStyle = "green";
-  ctx.fillRect(snake[0].x + 1, snake[0].y + 1, cellSize - 1, cellSize - 1);
-
+  // board
+  const cellSize = 10;
+  const cellsGrid = 20;
   // apple's initial position
   let appleX = Math.floor(Math.random() * cellsGrid);
   let appleY = Math.floor(Math.random() * cellsGrid);
+  // snake's initial values
+  let snake = [{ x: 0, y: 0 }];
   
+  // draw snake
+  function drawSnake() {
+    ctx.fillStyle = 'green';
+    for (let i = 0 ; i < snake.length ; i++) {
+      ctx.fillRect(snake[i].x * cellSize + 1, snake[i].y * cellSize + 1, cellSize - 1, cellSize - 1);
+    }
+  }
+  drawSnake();
+
   // apple interval: random position change every 10 seconds
   function setApple(time) {
     drawApple(appleX, appleY);
@@ -24,7 +31,7 @@
     }, time);
     return appleInterval;
   }
-  setApple(2000);
+  setApple(10000);
   
   // draw apple
   function drawApple(x, y) {
@@ -37,7 +44,7 @@
       Math.PI * 2,
       true
     );
-    ctx.fillStyle = "red";
+    ctx.fillStyle = 'red';
     ctx.fill();
   }
 })();
