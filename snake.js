706c47d66620fd380414191c12bc9fe20a87ad24
Tomek_M <tomasz.milaszewski@gmail.com>
2018-12-06 11:45:51 +0100

    adds starting game on 'Space' press and prepares gameOver function

--- snake.js
+++ snake.js
@@ -1,126 +1,152 @@
 (function game() {
-  // canvas
-  const canvas = document.querySelector("#canvas");
-  const ctx = canvas.getContext("2d");
-  // board
-  const cellSize = 10;
-  const cellsGrid = 20;
-  // apple's initial values
-  const appleTime = 10000;
-  let appleX = Math.floor(Math.random() * cellsGrid);
-  let appleY = Math.floor(Math.random() * cellsGrid);
-  var appleInterval;
-  // snake's initial values
-  const snakeTime = 200;
-  let snake = [{ x: 3, y: 0 }, { x: 2, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 0 }];
-  let currentHeadX = 3;
-  let currentHeadY = 0;
-  let incrX = 1;
-  let incrY = 0;
-  var snakeInterval;
+  let spaceCounter = 0;
+  document.addEventListener("keydown", e => {
+    if (e.keyCode === 32) {
+      spaceCounter === 0 ? action() : ((spaceCounter % 2 !== 0) ? pauseOn() : pauseOff())
+      spaceCounter ++;
+    }
+  });
 
-  // snake interval
-  function moveSnake(time) {
-    drawSnake();
-    snakeInterval = setInterval(() => {
-      clearSnake();
-      currentHeadX += incrX;
-      currentHeadY += incrY;
-      snake.unshift({x: currentHeadX, y: currentHeadY});
-      eatApple();
+  // TODO
+  // function pauseOn() {}
+  // function pauseOff() {}
+
+  function gameOver() {
+    clearInterval(snakeInterval);
+    clearInterval(appleInterval);
+    document.querySelector('game-message').innerHTML = 'Game over. Press Space to replay';
+    spaceCounter = 0;
+  }
+
+  function action() {
+    // canvas
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext("2d");
+    // board
+    const cellSize = 10;
+    const cellsGrid = 20;
+    // apple's initial values
+    const appleTime = 10000;
+    let appleX = Math.floor(Math.random() * cellsGrid);
+    let appleY = Math.floor(Math.random() * cellsGrid);
+    var appleInterval;
+    // snake's initial values
+    const snakeTime = 200;
+    let snake = [
+      { x: 3, y: 0 },
+      { x: 2, y: 0 },
+      { x: 1, y: 0 },
+      { x: 0, y: 0 }
+    ];
+    let currentHeadX = 3;
+    let currentHeadY = 0;
+    let incrX = 1;
+    let incrY = 0;
+    var snakeInterval;
+
+    // snake interval
+    function moveSnake(time) {
       drawSnake();
-    }, time);
-    return snakeInterval;
-  }
-  moveSnake(snakeTime);
-
-  // eat apple
-  function eatApple() {
-    if (currentHeadX === appleX && currentHeadY === appleY) {
-      clearInterval(appleInterval);
-      appleX = Math.floor(Math.random() * cellsGrid);
-      appleY = Math.floor(Math.random() * cellsGrid);
-      setApple(appleTime);
-    } else {
-      snake.pop();
+      snakeInterval = setInterval(() => {
+        clearSnake();
+        currentHeadX += incrX;
+        currentHeadY += incrY;
+        snake.unshift({ x: currentHeadX, y: currentHeadY });
+        eatApple();
+        drawSnake();
+      }, time);
+      return snakeInterval;
     }
-  }
+    moveSnake(snakeTime);
 
-  // clear snake
-  function clearSnake() {
-    for (let i = 0; i < snake.length; i++) {
-      ctx.clearRect(
-        snake[i].x * cellSize,
-        snake[i].y * cellSize,
-        cellSize,
-        cellSize
-      );
+    // eat apple
+    function eatApple() {
+      if (currentHeadX === appleX && currentHeadY === appleY) {
+        clearInterval(appleInterval);
+        appleX = Math.floor(Math.random() * cellsGrid);
+        appleY = Math.floor(Math.random() * cellsGrid);
+        setApple(appleTime);
+      } else {
+        snake.pop();
+      }
     }
-  }
 
-  // draw snake
-  function drawSnake() {
-    ctx.fillStyle = "green";
-    for (let i = 0; i < snake.length; i++) {
-      ctx.fillRect(
-        snake[i].x * cellSize + 1,
-        snake[i].y * cellSize + 1,
-        cellSize - 1,
-        cellSize - 1
-      );
+    // clear snake
+    function clearSnake() {
+      for (let i = 0; i < snake.length; i++) {
+        ctx.clearRect(
+          snake[i].x * cellSize,
+          snake[i].y * cellSize,
+          cellSize,
+          cellSize
+        );
+      }
     }
-  }
 
-  // move listener
-  document.addEventListener("keydown", moveHandler);
-
-  // move handler
-  function moveHandler(e) {
-    switch (e.keyCode) {
-      case 37:
-        incrX = -1;
-        incrY = 0;
-        break;
-      case 38:
-        incrX = 0;
-        incrY = -1;
-        break;
-      case 39:
-        incrX = 1;
-        incrY = 0;
-        break;
-      case 40:
-        incrX = 0;
-        incrY = 1;
-        break;
+    // draw snake
+    function drawSnake() {
+      ctx.fillStyle = "green";
+      for (let i = 0; i < snake.length; i++) {
+        ctx.fillRect(
+          snake[i].x * cellSize + 1,
+          snake[i].y * cellSize + 1,
+          cellSize - 1,
+          cellSize - 1
+        );
+      }
     }
-  }
 
-  // apple interval: random position change every 10 seconds
-  function setApple(time) {
-    drawApple(appleX, appleY);
-    appleInterval = setInterval(() => {
-      ctx.clearRect(appleX * cellSize, appleY * cellSize, cellSize, cellSize);
-      appleX = Math.floor(Math.random() * cellsGrid);
-      appleY = Math.floor(Math.random() * cellsGrid);
+    // move listener
+    document.addEventListener("keydown", moveHandler);
+
+    // move handler
+    function moveHandler(e) {
+      switch (e.keyCode) {
+        case 37:
+          incrX = -1;
+          incrY = 0;
+          break;
+        case 38:
+          incrX = 0;
+          incrY = -1;
+          break;
+        case 39:
+          incrX = 1;
+          incrY = 0;
+          break;
+        case 40:
+          incrX = 0;
+          incrY = 1;
+          break;
+      }
+    }
+
+    // apple interval: random position change every 10 seconds
+    function setApple(time) {
       drawApple(appleX, appleY);
-    }, time);
-    return appleInterval;
-  }
-  setApple(appleTime);
+      appleInterval = setInterval(() => {
+        ctx.clearRect(appleX * cellSize, appleY * cellSize, cellSize, cellSize);
+        appleX = Math.floor(Math.random() * cellsGrid);
+        appleY = Math.floor(Math.random() * cellsGrid);
+        drawApple(appleX, appleY);
+      }, time);
+      return appleInterval;
+    }
+    setApple(appleTime);
 
-  // draw apple
-  function drawApple(x, y) {
-    ctx.beginPath();
-    ctx.arc(
-      x * cellSize + cellSize / 2,
-      y * cellSize + cellSize / 2,
-      cellSize / 2,
-      0,
-      Math.PI * 2,
-      true
-    );
-    ctx.fillStyle = "red";
-    ctx.fill();
+    // draw apple
+    function drawApple(x, y) {
+      ctx.beginPath();
+      ctx.arc(
+        x * cellSize + cellSize / 2,
+        y * cellSize + cellSize / 2,
+        cellSize / 2,
+        0,
+        Math.PI * 2,
+        true
+      );
+      ctx.fillStyle = "red";
+      ctx.fill();
+    }
   }
 })();
